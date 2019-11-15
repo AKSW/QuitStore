243c22429262fbf65c114b606c21fdcdec4da28d
Natanael Arndt <arndtn@gmail.com>
2019-11-15 16:56:08 +0100

    Fix usage of prov:hadRole property

--- quit/provenance.py
+++ quit/provenance.py
@@ -68,7 +68,7 @@ class Blame(object):
                         quit:updates ?update ;
                         quit:hex ?hex .
                 ?qa     prov:agent ?user ;
-                        prov:role quit:Author .
+                        prov:hadRole quit:Author .
                 ?user   foaf:mbox ?email ;
                         rdfs:label ?name .
                 ?update quit:graph ?context ;
