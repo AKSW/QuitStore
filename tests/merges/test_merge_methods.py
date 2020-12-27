ea51d9557b20c40f7bd15050d3da51193e7335dd
Simaris <sebastianz541@googlemail.com>
2020-12-27 11:03:09 +0100

    incomplete Tests for merge methods

--- tests/merges/test_merge_methods.py
+++ tests/merges/test_merge_methods.py
@@ -2,12 +2,14 @@ from context import quit
 
 import os
 from os import listdir
-from os.path import isdir, join
+from os.path import isfile, isdir, join
 import quit.application as quitApp
 from quit.web.app import create_app
 import unittest
 import pygit2
 from helpers import TemporaryRepositoryFactory
+import rdflib
+from atomicgraphs.atomic_graph import AtomicGraphFactory as aGraphFactory
 
 
 class GraphMergeTests(unittest.TestCase):
@@ -22,12 +24,18 @@ class GraphMergeTests(unittest.TestCase):
     def testThreeWayMerge(self):
         """Test merging two commits."""
         testPath = os.path.dirname(os.path.abspath(__file__))
-        print(testPath)
-        for d in listdir(testPath):
+        # for d in listdir(testPath):
+        # for d in ["TestA", "TestHouseMerge", "TestABCD", "TestB", "TestC"]:
+        #     if isdir(join(testPath, d)) and d != "__pycache__":
+        #         self._merge_test(d, "three-way")
+        for d in ["TestD"]:
             if isdir(join(testPath, d)) and d != "__pycache__":
-                self._prepare_merge_test(d, "three-way")
+                print("#######################################")
+                print("###              {}              ###".format(d))
+                print("#######################################")
+                self._merge_test(d, "three-way")
 
-    def _prepare_merge_test(self, dirPath, method):
+    def _merge_test(self, dirPath, method):
         # Prepate a git Repository
         file = open(join(dirPath, "base.nt"), "r")
         content = file.read()
@@ -51,9 +59,39 @@ class GraphMergeTests(unittest.TestCase):
             reference = repo.lookup_reference('refs/heads/%s' % "componentB")
             branchOid = reference.resolve().target
             branchCommit = repo.get(branchOid)
-            file = open(join(dirPath, "result.nt"), "r")
-            self.assertEqual(branchCommit.tree["graph.nt"].data.decode("utf-8"), file.read())
-            file.close()
+            if isfile(join(dirPath, "a_graphs")):
+                file = open(join(dirPath, "a_graphs"), "r")
+                aControllGraphContents = file.read().split("---")
+                file.close()
+                resultContent = branchCommit.tree["graph.nt"].data.decode("utf-8")
+                resultGraph = rdflib.Graph().parse(data=resultContent, format="nt")
+                aResultGraphs = set(iter(aGraphFactory(resultGraph)))
+                print("ResultContent:\n{}\n-----".format(resultContent))
+                print("Current Result Set:\n-->{}".format({a.__hash__() for a in aResultGraphs}))
+                for aControllGraphContent in aControllGraphContents:
+                    graph = rdflib.Graph().parse(data=aControllGraphContent, format="nt")
+                    for aGraph in aGraphFactory(graph):
+                        print("aGraph: {}".format(aGraph.__hash__()))
+                        message = "Merge test {}:\n    Graph {} is not in the set: {}"
+                        resultSetString = {a.__hash__() for a in aResultGraphs}
+                        message = message.format(dirPath, aGraph.__hash__(), resultSetString)
+                        try:
+                            self.assertTrue(aGraph in aResultGraphs, message)
+                        except AssertionError:
+                            graphFile = open(join(dirPath, "debugResult"), "w")
+                            graphFile.write(self.__show_comparison(aGraph, aControllGraphContent))
+                            graphFile.close()
+                            print("- {}".format(self.__show_colours(next(iter(aResultGraphs)))))
+                            print("- {}".format(self.__show_colours(aGraph)))
+                            raise
+                        aResultGraphs.remove(aGraph)
+                message = "Merge test {}:\n    Not all graphs were defined in a_graphs: {}"
+                message = message.format(dirPath, aResultGraphs)
+                self.assertEqual(0, len(aResultGraphs), message)
+            else:
+                file = open(join(dirPath, "result.nt"), "r")
+                self.assertEqual(branchCommit.tree["graph.nt"].data.decode("utf-8"), file.read())
+                file.close()
 
     def expand_branch(self, repo, branch, graphFile):
         reference = repo.lookup_reference('refs/heads/%s' % branch)
@@ -69,6 +107,46 @@ class GraphMergeTests(unittest.TestCase):
                                           "this is a test", treeOID, [branchOid])
         repo.state_cleanup()
         return newCommitOid
+
+    # TODO remove
+    def __show_comparison(self, graph, controllContent):
+        listLabel = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+        bNodeMap = {}
+        for triple in graph:
+            node = triple[0]
+            if node.n3() not in bNodeMap:
+                if isinstance(node, rdflib.BNode):
+                    bNodeMap[node.n3()] = "_:{}".format(listLabel.pop(0))
+                else:
+                    bNodeMap[node.n3()] = "<{}>".format(node.n3())
+            node = triple[2]
+            if node.n3() not in bNodeMap:
+                if isinstance(node, rdflib.BNode):
+                    bNodeMap[node.n3()] = "_:{}".format(listLabel.pop(0))
+                else:
+                    bNodeMap[node.n3()] = "<{}>".format(node.n3())
+        template = "{1}"
+        result = ""
+        for triple in graph:
+            newLine = "{} <{}> {} .".format(bNodeMap[triple[0].n3()],
+                                            triple[1], bNodeMap[triple[2].n3()])
+            result = template.format(result, newLine)
+            template = "{0}\n{1}"
+        return result
+
+    def __show_colours(self, graph):
+        bNodeSet = set()
+        for triple in graph:
+            if isinstance(triple[0], rdflib.BNode):
+                bNodeSet.add(triple[0])
+            if isinstance(triple[2], rdflib.BNode):
+                bNodeSet.add(triple[2])
+        colourSet = set(graph.colourPartitions[x] for x in bNodeSet)
+        print("===")
+        for node in bNodeSet:
+            print("node  {}".format(graph.colourPartitions[node]))
+        return sorted(colourSet)
+
 
 #    def testContextMerge(self):
 #        """Test merging two commits."""
