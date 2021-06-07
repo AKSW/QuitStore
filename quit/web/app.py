4b498ce2154faaaf1ded4ad9d1284ce0d3371106
BonaBeavis <gordian.dziwis@gmail.com>
2021-06-07 22:32:54 +0200

    Fix test for updated rdflib
    Signed-off-by: BonaBeavis <gordian.dziwis@gmail.com>

--- quit/web/app.py
+++ quit/web/app.py
@@ -137,8 +137,8 @@ def register_app(app, arguments):
     quit = Quit(config, repository, MemoryStore(bindings))
     quit.syncAll()
 
-    # content = quit.store.store.serialize(format='trig').decode()
-    # logger.debug("Initialize store with following content: {}".format(content))
+    content = quit.store.store.serialize(format='trig')
+    logger.debug("Initialize store with following content: {}".format(content))
 
     app.config['quit'] = quit
     app.config['blame'] = Blame(quit)
