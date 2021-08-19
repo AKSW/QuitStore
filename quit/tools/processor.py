b78cc67e8d71281f2e587ccbc0963b35d2bad13e
Natanael Arndt <arndtn@gmail.com>
2021-08-19 12:56:24 +0200

    Remove forked code from processor, monkey patch instead

--- quit/tools/processor.py
+++ quit/tools/processor.py
@@ -1,40 +1,13 @@
-from rdflib.query import Processor, Result, UpdateProcessor
-from rdflib.plugins.sparql.sparql import Query
-from rdflib.plugins.sparql.parser import parseQuery, parseUpdate
-
 from quit.tools.algebra import translateQuery, translateUpdate
 from quit.tools.evaluate import evalQuery
 from quit.tools.update import evalUpdate
 
-class SPARQLUpdateProcessor(UpdateProcessor):
-    def __init__(self, graph):
-        self.graph = graph
+import rdflib.plugins.sparql.processor
 
-    def update(self, strOrQuery, initBindings={}, initNs={}):
-        if isinstance(strOrQuery, str):
-            strOrQuery=translateUpdate(parseUpdate(strOrQuery), initNs=initNs)
+rdflib.plugins.sparql.processor.evalQuery = evalQuery
+rdflib.plugins.sparql.processor.evalUpdate = evalUpdate
+rdflib.plugins.sparql.processor.translateQuery = translateQuery
+rdflib.plugins.sparql.processor.translateUpdate = translateUpdate
 
-        return evalUpdate(self.graph, strOrQuery, initBindings)
-
-
-class SPARQLProcessor(Processor):
-
-    def __init__(self, graph):
-        self.graph = graph
-
-    def query(
-            self, strOrQuery, initBindings={},
-            initNs={}, base=None, DEBUG=False):
-        """
-        Evaluate a query with the given initial bindings, and initial
-        namespaces. The given base is used to resolve relative URIs in
-        the query and will be overridden by any BASE given in the query.
-        """
-
-        if not isinstance(strOrQuery, Query):
-            parsetree = parseQuery(strOrQuery)
-            query = translateQuery(parsetree, base, initNs)
-        else:
-            query = strOrQuery
-
-        return evalQuery(self.graph, query, initBindings, base)
+SPARQLUpdateProcessor = rdflib.plugins.sparql.processor.SPARQLUpdateProcessor
+SPARQLProcessor = rdflib.plugins.sparql.processor.SPARQLProcessor
