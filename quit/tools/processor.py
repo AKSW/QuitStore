5c89dd1a3f858855076a7f57e0a897a1a426ebc7
Natanael Arndt <arndtn@gmail.com>
2021-08-24 08:46:40 +0200

    don't monkey patch standard modules

--- quit/tools/processor.py
+++ quit/tools/processor.py
@@ -1,13 +1,8 @@
 from quit.tools.update import evalUpdate
-from rdflib.plugins.sparql.algebra import translateQuery, translateUpdate
-from rdflib.plugins.sparql.evaluate import evalQuery
 
 import rdflib.plugins.sparql.processor
 
-rdflib.plugins.sparql.processor.evalQuery = evalQuery
 rdflib.plugins.sparql.processor.evalUpdate = evalUpdate
-rdflib.plugins.sparql.processor.translateQuery = translateQuery
-rdflib.plugins.sparql.processor.translateUpdate = translateUpdate
 
 SPARQLUpdateProcessor = rdflib.plugins.sparql.processor.SPARQLUpdateProcessor
 SPARQLProcessor = rdflib.plugins.sparql.processor.SPARQLProcessor
